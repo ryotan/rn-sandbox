d78b01689a4874a8e236bd8a1e4fd8a797296187
ryotan <ryotan@ucb-jp.net>
2023-04-04 13:11:01 +0900

    🚚: app.json -> app.config.js

---
+++ app.config.js
@@ -0,0 +1,31 @@
+export default {
+  expo: {
+    name: 'RN Sandbox',
+    slug: 'rn-sandbox',
+    version: '0.1.0',
+    orientation: 'default',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'pw.itr0.rn.sandbox',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      package: 'pw.itr0.rn.sandbox',
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    plugins: ['./configs/plugins/withXcode143Workaround', 'expo-build-properties', 'expo-localization'],
+  },
+};
