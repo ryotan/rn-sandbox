18fedff1b5d01a4693665a6d4a730660c2393944
ryotan <ryotan@ucb-jp.net>
2023-04-12 02:52:50 +0900

    🥅: Add error handler

---
+++ src/bases/core/errors/ErrorWithErrorCode.ts
@@ -0,0 +1,4 @@
+export type ErrorWithErrorCode = Error & {errorCode: string};
+
+export const isErrorWithErrorCode = (error: unknown): error is ErrorWithErrorCode =>
+  error != null && error instanceof Error && 'errorCode' in error;
