5a20657959b9df60268c285b241396266676b170
ryotan <ryotan@ucb-jp.net>
2023-04-19 21:09:36 +0900

    🎨: Polish test code

--- src/bases/core/errors/ApplicationError.test.ts
+++ src/bases/core/errors/ApplicationError.test.ts
@@ -13,29 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {ApplicationError, isApplicationError} from './ApplicationError';
+import {AssertionError} from '@bases/core/utils';
+
+import {ApplicationError, assertApplicationError, isApplicationError} from './ApplicationError';
 import {ErrorWithErrorCode} from './ErrorWithErrorCode';
 
 class ApplicationErrorSubClass extends ApplicationError {}
 class SomeError extends ErrorWithErrorCode {}
 
 describe('isApplicationError', () => {
-  it('should return false if null', () => {
-    expect(isApplicationError(null)).toBe(false);
+  it.each([[null], [undefined], [{}], [new SomeError()]])('should return false if arg=[%s]', arg => {
+    expect(isApplicationError(arg)).toBe(false);
+    expect(() => assertApplicationError(arg)).toThrow(AssertionError);
   });
-  it('should return false if undefined', () => {
-    expect(isApplicationError(undefined)).toBe(false);
-  });
-  it('should return false if object but not instance of ApplicationError', () => {
-    expect(isApplicationError({})).toBe(false);
-  });
-  it('should return true if ApplicationError', () => {
-    expect(isApplicationError(new ApplicationError())).toBe(true);
-  });
-  it('should return true if sub class of ApplicationError', () => {
-    expect(isApplicationError(new ApplicationErrorSubClass())).toBe(true);
-  });
-  it('should return false if another sub class of ErrorWithErrorCode', () => {
-    expect(isApplicationError(new SomeError())).toBe(false);
+
+  it.each([[new ApplicationError()], [new ApplicationErrorSubClass()]])('should return true if arg=[%s]', arg => {
+    expect(isApplicationError(arg)).toBe(true);
+    expect(() => assertApplicationError(arg)).not.toThrow(AssertionError);
   });
 });
