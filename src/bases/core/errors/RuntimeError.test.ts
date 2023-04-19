3750f075b90321d1024e820cd93067a8f9ebcfd2
Ryo Tanaka <ryotan@ucb-jp.net>
2023-04-20 00:23:00 +0900

    🔊: Improve nested error stacktrace message (#19)
    ## 🤔 What was the reason for the change
    
    Due to the difficulty in identifying nested errors from error messages,
    I have decided to prepend `Caused by: ` at the beginning of such errors
    that are nested. This will help improve the readability and
    comprehension of error messages for native programmers.
    
    ## ✅ What's changed
    
    - [x] Prepend `Caused by: ` at the beginning of such errors that are
    nested.
    - [x] Log stacktrace on error.
    - [x] Polish test code.

--- src/bases/core/errors/RuntimeError.test.ts
+++ src/bases/core/errors/RuntimeError.test.ts
@@ -13,29 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import {AssertionError} from '@bases/core/utils';
+
 import {ErrorWithErrorCode} from './ErrorWithErrorCode';
-import {RuntimeError, isRuntimeError} from './RuntimeError';
+import {RuntimeError, assertRuntimeError, isRuntimeError} from './RuntimeError';
 
 class RuntimeErrorSubClass extends RuntimeError {}
 class SomeError extends ErrorWithErrorCode {}
 
 describe('isRuntimeError', () => {
-  it('should return false if null', () => {
-    expect(isRuntimeError(null)).toBe(false);
+  it.each([[null], [undefined], [{}], [new SomeError()]])('should return false if arg=[%s]', arg => {
+    expect(isRuntimeError(arg)).toBe(false);
+    expect(() => assertRuntimeError(arg)).toThrow(AssertionError);
   });
-  it('should return false if undefined', () => {
-    expect(isRuntimeError(undefined)).toBe(false);
-  });
-  it('should return false if object but not instance of RuntimeError', () => {
-    expect(isRuntimeError({})).toBe(false);
-  });
-  it('should return true if RuntimeError', () => {
-    expect(isRuntimeError(new RuntimeError())).toBe(true);
-  });
-  it('should return true if sub class of RuntimeError', () => {
-    expect(isRuntimeError(new RuntimeErrorSubClass())).toBe(true);
-  });
-  it('should return false if another sub class of ErrorWithErrorCode', () => {
-    expect(isRuntimeError(new SomeError())).toBe(false);
+
+  it.each([[new RuntimeError()], [new RuntimeErrorSubClass()]])('should return true if arg=[%s]', arg => {
+    expect(isRuntimeError(arg)).toBe(true);
+    expect(() => assertRuntimeError(arg)).not.toThrow(AssertionError);
   });
 });
