e385fed29b6103e20020609ff70c02e00c6801b5
ryotan <ryotan@ucb-jp.net>
2023-05-03 22:08:54 +0900

    ✨: Distinguish timeout and cancel on http request aborted

--- src/bases/http-client/httpCall.ts
+++ src/bases/http-client/httpCall.ts
@@ -4,6 +4,7 @@ import {applicationName, nativeApplicationVersion} from 'expo-application';
 import {Platform} from 'react-native';
 
 import {RequestCancelledError} from './RequestCancelledError';
+import {RequestTimeoutError} from './RequestTimeoutError';
 
 export type ErrorType<ErrorResponseBody> = AxiosError<ErrorResponseBody> | RequestCancelledError;
 
@@ -19,24 +20,38 @@ const AXIOS_INSTANCE = Axios.create({baseURL, headers});
 export const httpCall = <ResponseBody>(config: AxiosRequestConfig): Promise<AxiosResponse<ResponseBody>> => {
   const timeout = config.timeout ?? REQUEST_TIMEOUT;
   let timerId: NodeJS.Timeout;
+  let timeoutAbort = false;
   if (config.signal == null) {
     // Cannot use "AbortSignal.timeout" because it is not supported by React Native (Hermes).
     const controller = new AbortController();
-    timerId = setTimeout(() => controller.abort(), timeout);
+    timerId = setTimeout(() => {
+      controller.abort();
+      timeoutAbort = true;
+    }, timeout);
     config.signal = controller.signal;
   }
 
   return AXIOS_INSTANCE<ResponseBody>(config)
     .catch(error => {
       if (Axios.isCancel(error)) {
-        throw new RequestCancelledError(
-          'The request has been cancelled due to a timeout or has been aborted by client.' +
-            ` method=[${String(config.method)}] url=[${String(config.url)}] timeout=[${timeout}ms]`,
-          error,
-          'RequestCancelledError',
-        );
+        if (timeoutAbort) {
+          throw new RequestTimeoutError(
+            `The request has been cancelled due to a timeout. ${parameters(config, timeout)}`,
+            error,
+            'RequestTimeoutError',
+          );
+        } else {
+          throw new RequestCancelledError(
+            `The request has been cancelled by the client. ${parameters(config, timeout)}`,
+            error,
+            'RequestCancelledError',
+          );
+        }
       }
       throw error;
     })
     .finally(() => clearTimeout(timerId));
 };
+
+const parameters = (config: AxiosRequestConfig, timeout: number) =>
+  `method=[${String(config.method)}] url=[${String(config.url)}] timeout=[${timeout}ms]`;
