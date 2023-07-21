a64b18847224475ba5da6a95a15ba25f4091f4ae
ryotan <ryotan@ucb-jp.net>
2023-07-22 05:31:31 +0900

    ⬆️: Update jest setting of React Native Reanimated

--- __jest__/setup/react-native-reanimated.js
+++ __jest__/setup/react-native-reanimated.js
@@ -1,7 +1,2 @@
-// jest.advanceTimersByTimeを使用した際に、TypeError: Cannot read properties of undefined (reading 'now')が発生してしまう問題に対応
-// https://github.com/software-mansion/react-native-reanimated/issues/3125
-global.ReanimatedDataMock = {
-  now: () => Date.now(),
-};
 // https://docs.swmansion.com/react-native-reanimated/docs/guide/testing/
-require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+require('react-native-reanimated').setUpTests();
